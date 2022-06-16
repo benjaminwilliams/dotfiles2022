4a13e673dceb87aae83b1a60a4f576746d5589d4
Ben Williams <benj@minwilliams.net>
2022-06-16 22:10:38 +1000

    update from woolsey, remove packer file

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,15 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+# polybar example 2>&1 | tee -a /tmp/polybar.log & disown
 
-echo "Polybar launched..."
+# echo "Polybar launched..."
+
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
